db2a62cb6ec62e8ea261b67639ddf412190ebc20
ak4721269 <akash12bhardwaj@gmail.com>
2025-04-02 16:07:11 +0530

    Update outfit_transformer.py

--- outfit_transformer/models/outfit_transformer.py
+++ outfit_transformer/models/outfit_transformer.py
@@ -29,6 +29,7 @@
 # from outfit_transformer.loss.triplet_loss import triplet_loss
 
 
+
 # class OutfitTransformer(nn.Module):
     
 #     def __init__(
@@ -427,6 +428,7 @@ from outfit_transformer.models.encoder.builder import *
 from outfit_transformer.loss.focal_loss import focal_loss
 from outfit_transformer.loss.bce_loss import bce_loss
 from outfit_transformer.loss.triplet_loss import triplet_loss
+from outfit_transformer.loss.asymmetric_focal_loss import asymmetric_focal_loss
 
 
 class OutfitTransformer(nn.Module):
@@ -548,7 +550,7 @@ class OutfitTransformer(nn.Module):
             inputs = {key: value.to(device) for key, value in batch['inputs'].items()}
 
             logits = self.cp_forward(inputs, do_encode=True)
-            loss = bce_loss(logits, targets.to(device))
+            loss = asymmetric_loss(logits, targets.to(device))
         elif task == 'cir':
             # Randomly extract the number of items to be used as query and answer.
             n_outfit_per_batch = torch.sum(~batch['outfits']['mask'], dim=1).numpy()
