16c2fd399a83ee1527edfe626e2e8e277258a10a
ak4721269 <akash12bhardwaj@gmail.com>
2025-04-04 16:51:47 +0530

    Update train.py

--- train.py
+++ train.py
@@ -83,28 +83,28 @@ if __name__ == '__main__':
 
     if args.task == 'cp':
         train_dataset_args = DatasetArguments(
-            polyvore_split = 'disjoint',
+            polyvore_split = 'nondisjoint',
             task_type = 'cp',
             dataset_type = 'train',
             outfit_max_length=args.outfit_max_length,
             use_text=True
             )
         valid_dataset_args = DatasetArguments(
-            polyvore_split = 'disjoint',
+            polyvore_split = 'nondisjoint',
             task_type = 'cp',
             dataset_type = 'valid',
             outfit_max_length=args.outfit_max_length,
             use_text=True
             )
         valid_fitb_dataset_args = DatasetArguments(
-            polyvore_split = 'disjoint',
+            polyvore_split = 'nondisjoint',
             task_type = 'fitb',
             dataset_type = 'valid',
             outfit_max_length=12,
             use_text=True
             )
         valid_cp_dataset_args = DatasetArguments(
-            polyvore_split = 'disjoint',
+            polyvore_split = 'nondisjoint',
             task_type = 'cp',
             dataset_type = 'valid',
             outfit_max_length=12,
@@ -135,14 +135,14 @@ if __name__ == '__main__':
 
     elif args.task == 'cir':
         train_dataset_args = DatasetArguments(
-            polyvore_split = 'disjoint',
+            polyvore_split = 'nondisjoint',
             task_type = 'outfit',
             dataset_type = 'train',
             outfit_max_length=args.outfit_max_length,
             use_text=True
             )
         valid_dataset_args = DatasetArguments(
-            polyvore_split = 'disjoint',
+            polyvore_split = 'nondisjoint',
             task_type = 'outfit',
             dataset_type = 'valid',
             outfit_max_length=args.outfit_max_length,
